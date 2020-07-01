6cb6b0328d74d0d9222fe87faf0c05146610acc0
bukirwa99 <angellabukirwa99@gmail.com>
2020-07-01 22:15:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+gitimport React from 'react';
+
+
 
 function App() {
+  constructor(props){
+    super(props);{
+      this.state ={
+        newItem:""
+        list:[]
+      }
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        Add an Item---
+        <br/>
+        <input type="text"placeholder="Please enter item"Value "this.state.newItem"onChange={}
+      </div>
     </div>
+   
   );
 }
 
